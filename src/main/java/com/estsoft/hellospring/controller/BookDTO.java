ff5e13fe7219f7779a1511a6a1e095278b4133de
chc <311mikecho@gmail.com>
2024-03-05 17:45:12 +0900

    Spring-practice2

---
+++ src/main/java/com/estsoft/hellospring/controller/BookDTO.java
@@ -0,0 +1,16 @@
+package com.estsoft.hellospring.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookDTO {
+    private String id;
+    private String name;
+    private String author;
+}
